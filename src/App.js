d8bcfdb69ef0139f199373075a10b56623bcf243
Francisco Santana <9118289+frvncisco@users.noreply.github.com>
2021-05-02 21:52:58 -0400

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy(() => import('./pages/login'));
+
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path="/login" compoment={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
+
+/**
+ * Client-side rendered app: react (create-react-app)
+ * -> database using Firebase
+ * -> react-loading-skeleton
+ * -> tailwind css
+ *
+ * Folder structure:
+ * src
+ * -> components
+ * -> constants
+ * -> context
+ * -> helpers
+ * -> hooks
+ * -> pages
+ * -> lib (firebase lives here)
+ * -> services (firebase functions)
+ * -> styles (tailwind folder)
+ * --> app
+ * --> tailwind
+ */
